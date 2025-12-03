49a51c79fa299fb57929d37ee71904dfb753ea71
K3vnDev <kevinrpolo16@gmail.com>
2025-12-02 23:12:11 -0500

    Enhance 404 pages

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,6 +1,4 @@
-interface Props {
-  className?: string
-}
+type Props = React.SVGProps<SVGSVGElement>
 
 export const AboutIcon = (props: Props) => (
   <svg
@@ -537,5 +535,25 @@ export const CopyIcon = (props: Props) => (
     <path stroke='none' d='M0 0h24v24H0z' fill='none' />
     <path d='M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z' />
     <path d='M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1' />
+  </svg>
+)
+
+export const ArrowIcon = (props: Props) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    {...props}
+  >
+    <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+    <path d='M5 12l14 0' />
+    <path d='M5 12l6 6' />
+    <path d='M5 12l6 -6' />
   </svg>
 )
