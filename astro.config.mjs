7a97e01882cd9d62ba79b745c05e526dff133ddf
K3vnDev <kevinrpolo16@gmail.com>
2026-01-07 18:52:51 -0500

    Fix Vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import netlify from '@astrojs/netlify'
 import react from '@astrojs/react'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
+
+import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   vite: { plugins: [tailwindcss()] },
   integrations: [react()],
-  adapter: netlify()
+  adapter: vercel()
 })
