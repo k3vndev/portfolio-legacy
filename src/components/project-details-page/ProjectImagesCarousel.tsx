f1a0b080eb7c56c617857a80397fbbad6aab5d19
K3vnDev <kevinrpolo16@gmail.com>
2026-01-29 23:21:41 -0500

    Update CV and enhance project page animations

--- src/components/project-details-page/ProjectImagesCarousel.tsx
+++ src/components/project-details-page/ProjectImagesCarousel.tsx
@@ -1,9 +1,5 @@
 import { useResponsiveness } from '@/hooks/useResponsiveness'
-import {
-  Carousel,
-  CarouselItem,
-  NavigationArrows,
-} from '@k3vndev/react-carousel'
+import { Carousel, CarouselItem, NavigationArrows } from '@k3vndev/react-carousel'
 
 interface Props {
   projectName: string
@@ -14,31 +10,33 @@ export const ProjectImagesCarousel = ({ projectName, images }: Props) => {
   const { media } = useResponsiveness()
   const visibleItems = media.md ? 2 : 1
 
+  const getAnimationDelay = (index: number) => {
+    const baseDelay = 300 // milliseconds
+    const delay = baseDelay + index * 100
+    return `${delay}ms`
+  }
+
   return (
     <Carousel
       className={{
-        wrapper:
-          'max-w-full mt-5 animate-fade-in anim-delay-300 anim-scale-90 anim-ease-out-back',
+        wrapper: 'max-w-full mt-5 animate-appear anim-delay-200'
       }}
       itemsCount={images.length}
       gap={16}
       visibleItems={visibleItems}
-      navigationHandler={
-        <NavigationArrows
-          className={{ both: '[&.visible]:*:cursor-pointer' }}
-        />
-      }
+      navigationHandler={<NavigationArrows className={{ both: '[&.visible]:*:cursor-pointer' }} />}
     >
       {images.map((imgSrc, index) => (
         <CarouselItem
           key={index}
-          className='overflow-clip bg-white/5 flex text-white/50'
+          className='overflow-clip bg-white/5 flex text-white/50 border-2 border-white/5'
         >
           <img
-            className='size-full object-cover aspect-5/3'
+            className='size-full object-cover aspect-5/3 animate-fade-in anim-blur-xl anim-scale-125 anim-ease-out-sine anim-duration-500'
             src={imgSrc}
             alt={`Showcase of the project ${projectName}`}
             draggable={false}
+            style={{ animationDelay: getAnimationDelay(index) }}
           />
         </CarouselItem>
       ))}
